3dc0d93c75687ef3b0869cca3bbd0c2a69203ee6
Yogesh Patil <yogeshpatil432004@gmail.com>
2026-08-03 01:05:50 +0530

    Refactor create_embedding function and enhance JSON processing for chunk embeddings

--- read_chunks.py
+++ read_chunks.py
@@ -1,15 +1,41 @@
 import requests
+import os
+import json
+import pandas as pd
 
-
-def create_embedding(text):
-    r = requests.post("http://localhost:11434/api/embeddings", json={
+def create_embedding(text_list):
+    r = requests.post("http://localhost:11434/api/embed", json={
         "model": "bge-m3",
-        "prompt": text
+        "input": text_list
     })
 
-    embedding = r.json()['embedding']
+    embedding = r.json()['embeddings']
     return embedding
 
 
-a = create_embedding("Hello, world!")
-print(a)
+
+jsons = os.listdir("jsons")       # List all the jsons
+# print(jsons) 
+my_dict = []
+chunk_id = 0
+
+for json_file in jsons:
+    with open(f"jsons/{json_file}", "r") as f:
+        content = json.load(f)
+    print(f"Creating Embeddings for {json_file}...")
+    embeddings = create_embedding([c['text'] for c in content['chunks']])
+
+    for i, chunk in enumerate(content['chunks']):
+        chunk['id'] = chunk_id
+        chunk['embedding'] = embeddings[i]
+        chunk_id += 1
+        my_dict.append(chunk)
+        
+    
+# print(my_dict)
+
+df = pd.DataFrame.from_records(my_dict)
+print(df)
+
+# a = create_embedding("Hello, world!")
+# print(a)
